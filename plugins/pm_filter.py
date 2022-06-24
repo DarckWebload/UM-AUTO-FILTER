252f5e3e723fcc7b6882c782331c0bb76e915abb
DarckWebload <106021000+DarckWebload@users.noreply.github.com>
2022-06-24 16:38:52 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -827,29 +827,17 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
         await asyncio.sleep(0.5)
         reply2 = await reply1.edit_text(
-            text="■□□□□□"
-        )
-        await asyncio.sleep(0.5)
-        reply3 = await reply2.edit_text(
             text="■■□□□□"
         )
         await asyncio.sleep(0.5)
-        reply4 = await reply3.edit_text(
-            text="■■■□□□"
-        )
-        await asyncio.sleep(0.5)
-        reply5 = await reply4.edit_text(
+        reply3 = await reply2.edit_text(
             text="■■■■□□"
         )
         await asyncio.sleep(0.5)
-        reply6 = await reply5.edit_text(
-            text="■■■■■□"
-         )
-        await asyncio.sleep(0.5)
-        reply7 = await reply6.edit_text(
+        reply4 = await reply3.edit_text(
             text="■■■■■■"
          )
-        await reply7.delete()
+        await reply4.delete()
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.HELP_TXT.format(query.from_user.mention),
@@ -887,29 +875,17 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
         await asyncio.sleep(0.5)
         reply2 = await reply1.edit_text(
-            text="■□□□□□"
-        )
-        await asyncio.sleep(0.5)
-        reply3 = await reply2.edit_text(
             text="■■□□□□"
         )
         await asyncio.sleep(0.5)
-        reply4 = await reply3.edit_text(
-            text="■■■□□□"
-        )
-        await asyncio.sleep(0.5)
-        reply5 = await reply4.edit_text(
+        reply3 = await reply2.edit_text(
             text="■■■■□□"
         )
         await asyncio.sleep(0.5)
-        reply6 = await reply5.edit_text(
-            text="■■■■■□"
-         )
-        await asyncio.sleep(0.5)
-        reply7 = await reply6.edit_text(
+        reply4 = await reply3.edit_text(
             text="■■■■■■"
          )
-        await reply7.delete()
+        await reply4.delete()
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.HELP_TXT.format(query.from_user.mention),
