77eda87a768851714d2fe55b10a3cb8cfc2a4a11
DarckWebload <106021000+DarckWebload@users.noreply.github.com>
2022-06-30 11:48:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -201,28 +201,28 @@ async def next_page(bot, query):
         ]
     )
 
-    if 0 < offset <= 10:
+    if 0 < offset <= 6:
         off_set = 0
     elif offset == 0:
         off_set = None
     else:
-        off_set = offset - 10
+        off_set = offset - 6
     if n_offset == 0:
         btn.append(
             [InlineKeyboardButton("ʙᴀᴄᴋ", callback_data=f"next_{req}_{key}_{off_set}"),
-             InlineKeyboardButton(f"ᴘᴀɢᴇꜱ {round(int(offset) - 10) + 1} / {round(total - 10)}",
+             InlineKeyboardButton(f"ᴘᴀɢᴇꜱ {round(int(offset) - 6) + 1} / {round(total - 6)}",
                                   callback_data="pages")]
         )
     elif off_set is None:
         btn.append(
             [InlineKeyboardButton(text=f"ᴩᴀɢᴇꜱ", callback_data="pages"),
-             InlineKeyboardButton(f"{round(int(offset) - 10) + 1} - {round(total - 10)}", callback_data="pages"),
+             InlineKeyboardButton(f"{round(int(offset) - 6) + 1} - {round(total - 6)}", callback_data="pages"),
              InlineKeyboardButton("ɴᴇxᴛ", callback_data=f"next_{req}_{key}_{n_offset}")])
     else:
         btn.append(
             [
                 InlineKeyboardButton("ʙᴀᴄᴋ", callback_data=f"next_{req}_{key}_{off_set}"),
-                InlineKeyboardButton(f"{round(int(offset) - 10) + 1} - {round(total - 10)}", callback_data="pages"),
+                InlineKeyboardButton(f"{round(int(offset) 6) + 1} - {round(total - 6)}", callback_data="pages"),
                 InlineKeyboardButton("ɴᴇxᴛ", callback_data=f"next_{req}_{key}_{n_offset}")
             ],
         )
@@ -1406,7 +1406,7 @@ async def auto_filter(client, msg, spoll=False):
         req = message.from_user.id if message.from_user else 0
         btn.append(
             [InlineKeyboardButton(text=f"ᴩᴀɢᴇꜱ", callback_data="pages"),
-             InlineKeyboardButton(text=f"1 - {round(int(total_results) - 10)}", callback_data="pages"),
+             InlineKeyboardButton(text=f"1 - {round(int(total_results) - 6)}", callback_data="pages"),
              InlineKeyboardButton(text="ɴᴇxᴛ", callback_data=f"next_{req}_{key}_{offset}")]
         )
     else:
